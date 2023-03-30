69df43b10e5046cc51f3c7c3acca62fbfdd1ed4d
Simon Knittel <hallo@simonknittel.de>
2023-03-30 18:06:36 +0200

    refactor: Move stringToColor to Avatar component

--- app/src/components/Avatar.tsx
+++ app/src/components/Avatar.tsx
@@ -1,6 +1,17 @@
 import clsx from "clsx";
 import Image from "next/image";
-import { stringToColor } from "~/utils/stringToColor";
+
+// https://stackoverflow.com/questions/3426404/create-a-hexadecimal-colour-based-on-a-string-with-javascript/21682946#21682946
+export function stringToColor(str: string) {
+  let hash = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    hash = hash & hash;
+  }
+
+  return `hsl(${hash % 360}, 100%, 25%)`;
+}
 
 interface Props {
   className?: string;

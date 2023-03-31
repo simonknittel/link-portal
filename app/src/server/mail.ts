228dc531fdb6b5010de88a128d764924c723a5fe
Simon Knittel <hallo@simonknittel.de>
2023-03-31 11:51:45 +0200

    refactor: Improve types for mail.ts

--- app/src/server/mail.ts
+++ app/src/server/mail.ts
@@ -1,6 +1,7 @@
-import { type Project, type User } from "@prisma/client";
+import { type Project } from "@prisma/client";
 import * as formData from "form-data";
 import Mailgun from "mailgun.js";
+import { type Session } from "next-auth";
 import { env } from "~/env.mjs";
 
 const mailgun = new Mailgun(formData);
@@ -14,7 +15,7 @@ const mg = mailgun.client({
 export async function sendInviteEmail(
   to: string,
   project: Project,
-  invitee: User
+  invitee: Session["user"]
 ) {
   await mg.messages.create(env.MAILGUN_DOMAIN, {
     from: `Link Portal <noreply@${env.MAILGUN_DOMAIN}>`,
